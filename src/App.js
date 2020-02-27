5d12de602b9289c450bbadec68c198b291e3f526
mpuchyr <matt.puchyr@gmail.com>
2020-02-27 17:59:23 -0600

    Create componentDidMount() to fetch comic data in App.js

--- src/App.js
+++ src/App.js
@@ -11,95 +11,121 @@ const URL = "https://gateway.marvel.com/"
 const charactersUrl = URL + 'v1/public/characters'
 const comicsUrl = URL + 'v1/public/comics'
 
+let comics = []
+
 class App extends Component {
-  state = {
-    characterName: ''
-  }
-
-  handleOnClick = (event) => {
-
+  
+  componentDidMount() {
     let timeStamp = Date.now()
-
     let hash = md5(timeStamp + privateKey + publicKey)
-    // console.log(charactersUrl + `?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`)
-    return fetch(comicsUrl + `?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`)
+    fetch(comicsUrl + `?format=comic&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`)
     .then(res => {
       if (res.status !== 200) {
         throw new Error(res.status.text)
       } else {
         return res.json()
       }
-      })
-    .then(info => {
-      console.log(info)
     })
-    .catch(err => {
-      console.log(err)
-    })
-
-
+    .then(info => console.log(info))
+    .catch(err => console.log(err))
   }
+  
+  // state = {
+  //   characterName: ''
+  // }
 
-  handleOnChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+  // handleOnClick = (event) => {
 
-  handleOnSubmit = (event) => {
-    event.preventDefault()
-    let timeStamp = Date.now()
-    let hash = md5(timeStamp + privateKey + publicKey)
-    let charName = this.state.characterName
-    // console.log(charactersUrl + `?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`)
-    return fetch(charactersUrl + `?name=${charName}&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`)
-    .then(res => {
-      if (res.status !== 200) {
-        throw new Error(res.status.text)
-      } else {
-        return res.json()
-      }
-      })
-    .then(info => {
-      this.displayCharacterInfo(info)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
+  //   let timeStamp = Date.now()
 
-  displayCharacterInfo = (character) => {
-    let name = document.querySelector("#character-name")
-    let desc = document.querySelector("#character-description")
-    let imgContainer = document.querySelector("#character-thumbnail")
-    let footer = document.querySelector("#footer")
-    name.innerText = character.data.results[0].name
-    desc.innerText = character.data.results[0].description
-    imgContainer.innerHTML = `
-      <img src=${character.data.results[0].thumbnail.path + '.' + character.data.results[0].thumbnail.extension} alt=${character.data.results[0].name}/>
-    `
-    footer.innerHTML = `${character.attributionHTML}`
-    // console.log(character.data.results[0].name)
-    console.log(character)
-    console.log(character.data.results[0].comics.items[0])
-  }
+  //   let hash = md5(timeStamp + privateKey + publicKey)
+  //   // console.log(charactersUrl + `?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`)
+  //   return fetch(comicsUrl + `?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`)
+  //   .then(res => {
+  //     if (res.status !== 200) {
+  //       throw new Error(res.status.text)
+  //     } else {
+  //       return res.json()
+  //     }
+  //     })
+  //   .then(info => {
+  //     console.log(info)
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+
+
+  // }
+
+  // handleOnChange = (event) => {
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   })
+  // }
+
+  // handleOnSubmit = (event) => {
+  //   event.preventDefault()
+  //   let timeStamp = Date.now()
+  //   let hash = md5(timeStamp + privateKey + publicKey)
+  //   let charName = this.state.characterName
+  //   // console.log(charactersUrl + `?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`)
+  //   return fetch(charactersUrl + `?name=${charName}&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`)
+  //   .then(res => {
+  //     if (res.status !== 200) {
+  //       throw new Error(res.status.text)
+  //     } else {
+  //       return res.json()
+  //     }
+  //     })
+  //   .then(info => {
+  //     this.displayCharacterInfo(info)
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  // }
+
+  // displayCharacterInfo = (character) => {
+  //   let name = document.querySelector("#character-name")
+  //   let desc = document.querySelector("#character-description")
+  //   let imgContainer = document.querySelector("#character-thumbnail")
+  //   let footer = document.querySelector("#footer")
+  //   name.innerText = character.data.results[0].name
+  //   desc.innerText = character.data.results[0].description
+  //   imgContainer.innerHTML = `
+  //     <img src=${character.data.results[0].thumbnail.path + '.' + character.data.results[0].thumbnail.extension} alt=${character.data.results[0].name}/>
+  //   `
+  //   footer.innerHTML = `${character.attributionHTML}`
+  //   // console.log(character.data.results[0].name)
+  //   console.log(character)
+  //   console.log(character.data.results[0].comics.items[0])
+  // }
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <h1>Marvel App</h1>
+  //       <form onSubmit={this.handleOnSubmit}>
+  //         <input type="text" name="characterName" onChange={this.handleOnChange}></input>
+  //         <input type="submit"></input>
+  //       </form>
+
+  //       <h2 id="character-name"></h2>
+  //       <div id="character-thumbnail"></div>
+  //       <p id="character-description"></p>
+
+  //       <footer id="footer"></footer>
+  //     </div>
+  //   );
+  // }
 
   render() {
     return (
       <div className="App">
         <h1>Marvel App</h1>
-        <form onSubmit={this.handleOnSubmit}>
-          <input type="text" name="characterName" onChange={this.handleOnChange}></input>
-          <input type="submit"></input>
-        </form>
-
-        <h2 id="character-name"></h2>
-        <div id="character-thumbnail"></div>
-        <p id="character-description"></p>
-
-        <footer id="footer"></footer>
       </div>
-    );
+    )
   }
 
 }
