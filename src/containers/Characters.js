770473be07d8eb12c16eb12f4e77b86470d3cf45
mpuchyr <matt.puchyr@gmail.com>
2020-03-02 19:49:53 -0600

    Add state to containers/Characters.js

--- src/containers/Characters.js
+++ src/containers/Characters.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 class Characters extends Component {
     render() {
+        state = {
+            characters: [],
+            startsWith: ''
+        }
+
+
         return(
             <>
                 <h1>Placeholder Charcter Text</h1>
