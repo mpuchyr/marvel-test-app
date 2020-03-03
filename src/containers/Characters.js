56aa076bb289c861fae5047ce6dee5a6cedf5f3e
mpuchyr <matt.puchyr@gmail.com>
2020-03-02 19:57:02 -0600

    Create fetchCharacters in containers/Characters.js and call it with componentDidMount

--- src/containers/Characters.js
+++ src/containers/Characters.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
+import md5 from 'md5';
+
+const publicKey = process.env.REACT_APP_API_KEY
+const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
+const URL = "https://gateway.marvel.com/"
+const charsUrl = URL + 'v1/public/characters'
 
 class Characters extends Component {
-    render() {
-        state = {
-            characters: [],
-            startsWith: ''
+    state = {
+        characters: [],
+        startsWith: ''
+    }
+
+    componentDidMount = () => {
+        this.fetchCharacters()
+    }
+
+    fetchCharacters = () => {
+        let timeStamp = Date.now()
+        let hash = md5(timeStamp + privateKey + publicKey)
+        let fullUrl = ''
+        if (this.state.startsWith) {
+            fullUrl = charsUrl + `?nameStartsWith=${this.state.startsWith}&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+        } else {
+            fullUrl = charsUrl + `?&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
         }
+        fetch(fullUrl)
+        .then(res => {
+        if (res.status !== 200) {
+            throw new Error(res.status.text)
+        } else {
+            return res.json()
+        }
+        })
+        .then(info => {
+            console.log(info)
+            // this.setState({
+            //     characters: info.data.results
+            // })
+        })
+        .catch(err => console.log(err))
+    }
 
 
+    
+    render() {        
         return(
             <>
                 <h1>Placeholder Charcter Text</h1>
