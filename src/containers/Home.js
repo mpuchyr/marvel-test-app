c7f390a6f0ed66665b199fe57804b502ed030e2d
mpuchyr <matt.puchyr@gmail.com>
2020-03-03 19:41:34 -0600

    Add fetchComics, fetchCharacters, and fetchEvents to containers/Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import md5 from 'md5';
 
 const publicKey = process.env.REACT_APP_API_KEY
 const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
@@ -12,6 +13,70 @@ class Home extends Component {
         comics: [],
         characters: [],
         events: []
+    }
+
+    
+    fetchComics = () => {
+        let timeStamp = Date.now()
+        let hash = md5(timeStamp + privateKey + publicKey)
+        let fullUrl = comicsUrl + `?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+        fetch(fullUrl)
+        .then(res => {
+          if (res.status !== 200) {
+            throw new Error(res.status.text)
+          } else {
+            return res.json()
+          }
+        })
+        .then(info => {
+            this.setState({
+                ...this.state,
+                comics: info.data.results
+            })
+        })
+        .catch(err => console.log(err))
+    }
+
+    fetchCharacters = () => {
+        let timeStamp = Date.now()
+        let hash = md5(timeStamp + privateKey + publicKey)
+        let fullUrl = charsUrl + `?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+        fetch(fullUrl)
+        .then(res => {
+        if (res.status !== 200) {
+            throw new Error(res.status.text)
+        } else {
+            return res.json()
+        }
+        })
+        .then(info => {
+            console.log(info)
+            this.setState({
+                characters: info.data.results
+            })
+        })
+        .catch(err => console.log(err))
+    }
+
+    fetchEvents = () => {
+        let timeStamp = Date.now()
+        let hash = md5(timeStamp + privateKey + publicKey)
+        let fullUrl = eventsUrl + `?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+        fetch(fullUrl)
+        .then(res => {
+          if (res.status !== 200) {
+            throw new Error(res.status.text)
+          } else {
+            return res.json()
+          }
+        })
+        .then(info => {
+            console.log(info.data)
+            this.setState({
+                events: info.data.results
+            })
+        })
+        .catch(err => console.log(err))
     }
 
     render() {
