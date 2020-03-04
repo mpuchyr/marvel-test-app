c28361b410352e530e2229e7f92c1078b736dc5f
mpuchyr <matt.puchyr@gmail.com>
2020-03-03 19:36:04 -0600

    Add state to containers/Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
+const publicKey = process.env.REACT_APP_API_KEY
+const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
+const URL = "https://gateway.marvel.com/"
+const comicsUrl = URL + 'v1/public/comics'
+const eventsUrl = URL + 'v1/public/events'
+const charsUrl = URL + 'v1/public/characters'
+
 class Home extends Component {
+    state = {
+        comics: [],
+        characters: [],
+        events: []
+    }
+
     render() {
         return (
             <div>
