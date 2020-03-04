daac2cd346a40f54fa2b44eb9d01438af9565d57
mpuchyr <matt.puchyr@gmail.com>
2020-03-03 18:31:35 -0600

    Create fetchEvents in components/Events.js

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 import md5 from 'md5';
 
+const publicKey = process.env.REACT_APP_API_KEY
+const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
+const URL = "https://gateway.marvel.com/"
+const eventsUrl = URL + 'v1/public/events'
+
 class Events extends Component {
     state = {
         events: [],
         startsWith: '',
         limit: 20
     }
+
+    componentDidMount() {
+        this.fetchEvents()
+    }
+
+    fetchEvents = () => {
+        let timeStamp = Date.now()
+        let hash = md5(timeStamp + privateKey + publicKey)
+        let fullUrl = ''
+        if (this.state.startsWith) {
+            fullUrl = eventsUrl + `?nameStartsWith=${this.state.startsWith}&limit=${this.state.limit}&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+        } else {
+            fullUrl = eventsUrl + `?limit=${this.state.limit}&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+        }
+        fetch(fullUrl)
+        .then(res => {
+          if (res.status !== 200) {
+            throw new Error(res.status.text)
+          } else {
+            return res.json()
+          }
+        })
+        .then(info => {
+            console.log(info.data)
+            this.setState({
+                events: info.data.results
+            })
+        })
+        .catch(err => console.log(err))
+    }
+
+
     render(){
         return (
             <>
