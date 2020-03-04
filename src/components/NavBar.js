6082d537ca209d47fd88b68efc474d269cea65d5
mpuchyr <matt.puchyr@gmail.com>
2020-03-03 18:48:39 -0600

    Create NavLinks in components/NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import { NavLink } from 'react-router-dom'
 
 class NavBar extends Component {
 
     render() {
         return(
             <div className="navbar">
+                <NavLink to="/">Home</NavLink>
+
+                <NavLink to="/characters">Characters</NavLink>
+
+                <NavLink to="/comics">Comics</NavLink>
+
+                <NavLink to="/events">Events</NavLink>
             </div>
         )
     }
