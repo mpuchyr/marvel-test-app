d1b11419a06596ae654b269ed410586de4bfcdb9
mpuchyr <matt.puchyr@gmail.com>
2020-03-05 14:52:32 -0600

    Create state and componentDidMount in containers/Character.js

--- src/containers/Character.js
+++ src/containers/Character.js
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
 import md5 from 'md5';
 
+const publicKey = process.env.REACT_APP_API_KEY
+const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
+const URL = "https://gateway.marvel.com/"
+const charsUrl = URL + 'v1/public/characters'
+
 class Character extends Component {
+    state = {
+        characters: []
+    }
+
+
+    componentDidMount() {
+        this.fetchCharacter()
+    }
+
+    fetchCharacter = () => {
+        let timeStamp = Date.now()
+        let hash = md5(timeStamp + privateKey + publicKey)
+        let paramsId = parseInt(this.props.match.params.id, 10)
+        let fullUrl = charsUrl + `/${paramsId}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+        fetch(fullUrl)
+        .then(res => {
+            if (res.status !== 200) {
+                throw new Error(res.status.text)
+            } else {
+                return res.json()
+            }
+        })
+        .then(info => {
+            console.log(info)
+            this.setState({
+                characters: info.data.results
+            })
+        })
+        .catch(err => console.log(err))
+    }
+    
     render() {
         return(
             <div>
