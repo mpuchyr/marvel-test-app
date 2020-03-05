e4eb5ae65d379a2fedfb4b36cf669de5e38d2a29
mpuchyr <matt.puchyr@gmail.com>
2020-03-05 14:04:03 -0600

    Create fetchEvent in containers/Event.js using the id param to find a particular event

--- src/containers/Event.js
+++ src/containers/Event.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import md5 from 'md5';
+
+const publicKey = process.env.REACT_APP_API_KEY
+const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
+const URL = "https://gateway.marvel.com/"
+const eventsUrl = URL + 'v1/public/events'
 
 class Event extends Component {
+
+    fetchEvent = () => {
+        let timeStamp = Date.now()
+        let hash = md5(timeStamp + privateKey + publicKey)
+        let paramsId = parseInt(this.props.match.params.id, 10)
+        let fullUrl = eventsUrl + `${paramsId}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+    }
 
     render() {
         return (
