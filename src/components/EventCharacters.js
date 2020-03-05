a018983d6cb5ffca0b28f5745e4ce0c4ed372994
mpuchyr <matt.puchyr@gmail.com>
2020-03-05 16:42:57 -0600

    Create components/EventCharacters.js

---
+++ src/components/EventCharacters.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class EventCharacters extends Component {
+
+    render() {
+        return (
+            <h1>Event Character Placeholder</h1>
+        )
+    }
+}
+
+export default EventCharacters
