5e50fc7120c74855df09536269358c4cd937c656
mpuchyr <matt.puchyr@gmail.com>
2020-03-05 15:53:33 -0600

    Add state and create fetchComic in containers/Comic.js

--- src/containers/Comic.js
+++ src/containers/Comic.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
 
+const publicKey = process.env.REACT_APP_API_KEY
+const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
+const URL = "https://gateway.marvel.com/"
+const comicsUrl = URL + 'v1/public/comics'
+
 class Comic extends Component {
+    state = {
+        comics: []
+    }
+
+    fetchComic = () => {
+        let timeStamp = Date.now()
+        let hash = md5(timeStamp + privateKey + publicKey)
+        let paramsId = parseInt(this.props.match.params.id, 10)
+        let fullUrl = comicsUrl + `/${paramsId}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+        fetch(fullUrl)
+        .then(res => {
+            if (res.status !== 200) {
+                throw new Error(res.status.text)
+            } else {
+                return res.json()
+            }
+        })
+        .then(info => {
+            console.log(info)
+            this.setState({
+                characters: info.data.results
+            })
+        })
+        .catch(err => console.log(err))
+    }
+    }
+    
     render() {
         return(
             <div>
