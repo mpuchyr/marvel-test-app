85ba56efb9788d487ad5b34e31f97a6d7ed8eee9
mpuchyr <matt.puchyr@gmail.com>
2020-03-05 17:14:19 -0600

    Retrieve character id from character URI In getCharacterLi in components/EventCharacters.js

--- src/components/EventCharacters.js
+++ src/components/EventCharacters.js
@@ -6,13 +6,24 @@ const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
 const URL = "https://gateway.marvel.com/"
 const charsUrl = URL + 'v1/public/characters'
 
+let timeStamp = Date.now()
+let hash = md5(timeStamp + privateKey + publicKey)
+
 class EventCharacters extends Component {
-    state = {
-        characters: []
-    }
+
+    // getCharacterIds = () => {
+    //     let charIds = this.props.characters.forEach(character => {
+    //         let charURI = character.resourceURI.split("/")
+    //         let charId = charURI[charURI.length - 1]
+    //         return charId
+    //     })
+    // }
 
     getCharacterLi = () => {
         return this.props.characters.map(character => {
+            let charURI = character.resourceURI.split("/")
+            let charId = charURI[charURI.length - 1]
+            console.log(charId)
             console.log(character.name)
             return <li>{character.name}</li>
         })
@@ -31,6 +42,7 @@ class EventCharacters extends Component {
             <div>
                 <h1>Characters</h1>
                 { this.displayCharacters()}
+
             </div>
 
         )
