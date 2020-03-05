2bc35685cea899b183a53306f38797a13c2e3208
mpuchyr <matt.puchyr@gmail.com>
2020-03-05 16:53:13 -0600

    Add state to components/EventCharacters.js

--- src/components/EventCharacters.js
+++ src/components/EventCharacters.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import md5 from 'md5';
+
+const publicKey = process.env.REACT_APP_API_KEY
+const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
+const URL = "https://gateway.marvel.com/"
+const charsUrl = URL + 'v1/public/characters'
 
 class EventCharacters extends Component {
+    state = {
+        characters: []
+    }
 
     getCharacterLi = () => {
         return this.props.characters.map(character => {
