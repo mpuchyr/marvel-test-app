90f689ed1f6dbd849f7358dd088f3fff3b433167
mpuchyr <matt.puchyr@gmail.com>
2020-03-06 15:35:54 -0600

    Add state and necessary variables to components/CharacterComicImage.js

--- src/components/CharacterComicImage.js
+++ src/components/CharacterComicImage.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import md5 from 'md5';
+
+const publicKey = process.env.REACT_APP_API_KEY
+const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
+const URL = "https://gateway.marvel.com/"
+const charsUrl = URL + 'v1/public/characters'
 
 class CharacterComicImage extends Component {
+    state = {
+        imgSrc: '',
+        alt: ''
+    }
     render() {
         return (
             <div>
