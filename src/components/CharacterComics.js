ea66d848c3783f055b6e409cb4d48f845c9372fc
mpuchyr <matt.puchyr@gmail.com>
2020-03-06 15:27:02 -0600

    Fix errors in components/CharacterComicImage.js and components/CharacterComics.js

--- src/components/CharacterComics.js
+++ src/components/CharacterComics.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-// import CharacterComicImage from './CharacterComicImage'
+import CharacterComicImage from './CharacterComicImage'
 
 
 class CharacterComics extends Component {
     
-    // getComic = () => {
-    //     return this.props.comicss.map(comic => {
-    //         let comicURI = comic.resourceURI.split("/")
-    //         let comicId = charURI[charURI.length - 1]
-    //         let link = `/comics/${comicId}`
-    //         return (
-    //             <div className="event-character">
-    //                 <NavLink to={link} key={comic.name}>
-    //                     <EventCharacterImage comicId={comicId}/>
-    //                 </NavLink>
-    //             </div>
-    //         )
-    //     })
-    // }
+    getComic = () => {
+        return this.props.comics.map(comic => {
+            let comicURI = comic.resourceURI.split("/")
+            let comicId = comicURI[comicURI.length - 1]
+            let link = `/comics/${comicId}`
+            return (
+                <div className="event-character">
+                    <NavLink to={link} key={comic.name}>
+                        <CharacterComicImage comicId={comicId}/>
+                    </NavLink>
+                </div>
+            )
+        })
+    }
     
     render() {
         console.log(this.props.comics)
         return (
             <div>
                 <h1>Character Comics Placeholder</h1>
+                
             </div>
         )
     }
