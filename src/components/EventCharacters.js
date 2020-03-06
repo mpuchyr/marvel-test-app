36b11cce94a26b63e65e0491dbe6148880fd29cd
mpuchyr <matt.puchyr@gmail.com>
2020-03-05 22:21:42 -0600

    Create getCharacterImage in components/EventCharacters.js

--- src/components/EventCharacters.js
+++ src/components/EventCharacters.js
@@ -18,10 +18,30 @@ class EventCharacters extends Component {
             let charURI = character.resourceURI.split("/")
             let charId = charURI[charURI.length - 1]
             let link = `/characters/${charId}`
+            this.getCharacterImage(charId)
             console.log(charId)
             console.log(character.name)
             return <li><NavLink to={link}>{character.name}</NavLink></li>
         })
+    }
+
+    getCharacterImage = (charId) => {
+        let timeStamp = Date.now()
+        let hash = md5(timeStamp + privateKey + publicKey)
+        let fullUrl = charsUrl + `/${charId}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+        fetch(fullUrl)
+        .then(res => {
+            if (res.status !== 200) {
+                throw new Error(res.status.text)
+            } else {
+                return res.json()
+            }
+        })
+        .then(info => {
+            console.log(info)
+            
+        })
+        .catch(err => console.log(err))
     }
 
     displayCharacters = () => {
