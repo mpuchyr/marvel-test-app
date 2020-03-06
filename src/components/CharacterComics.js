0bdc619a2a01d42ec113d47770a2305c6227533f
mpuchyr <matt.puchyr@gmail.com>
2020-03-06 15:23:57 -0600

    Create components/CharacterComicImage.js and import it into components/CharacterComics.js

--- src/components/CharacterComics.js
+++ src/components/CharacterComics.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+// import CharacterComicImage from './CharacterComicImage'
 
 
 class CharacterComics extends Component {
     
-    getComic = () => {
-        return this.props.comicss.map(comic => {
-            let charURI = comic.resourceURI.split("/")
-            let charId = charURI[charURI.length - 1]
-            let link = `/characters/${charId}`
-            return (
-                <div className="event-character">
-                    <NavLink to={link} key={comic.name}>
-                        <EventCharacterImage charId={charId}/>
-                    </NavLink>
-                </div>
-            )
-        })
-    }
+    // getComic = () => {
+    //     return this.props.comicss.map(comic => {
+    //         let comicURI = comic.resourceURI.split("/")
+    //         let comicId = charURI[charURI.length - 1]
+    //         let link = `/comics/${comicId}`
+    //         return (
+    //             <div className="event-character">
+    //                 <NavLink to={link} key={comic.name}>
+    //                     <EventCharacterImage comicId={comicId}/>
+    //                 </NavLink>
+    //             </div>
+    //         )
+    //     })
+    // }
     
     render() {
         console.log(this.props.comics)
