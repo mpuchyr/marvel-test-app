348aa1f29bb7a83817dd4342ef9c22b53f131a9f
mpuchyr <matt.puchyr@gmail.com>
2020-03-06 22:41:16 -0600

    Change h1 text to Marvel Encyclopedia in App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Marvel App</h1>
+        <h1>Marvel Encyclopedia</h1>
           <div className="container">
             <Router>
               <NavBar />
